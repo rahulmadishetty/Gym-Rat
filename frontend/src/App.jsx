e96029a1f9c8ce883b342adbc6fe30d51d239cf3
rahul madishetty <rahul.madishetty@accenture.com>
2023-10-22 14:29:36 -0400

    Integration of Onboarding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@
 
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import { HOME, PROFILE, SIGN_IN, SIGN_UP } from './constants/routes';
+import { HOME, PROFILE, SIGN_IN, SIGN_UP, ONBOARDING } from './constants/routes';
 
 import SignUp from './pages/SignUp';
 import SignIn from "./pages/SignIn";
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Onboarding from './pages/Onboarding/mainonboarding'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path={PROFILE.INDEX} element={<Profile />} />
         <Route path={SIGN_UP.INDEX} element={<SignUp />} />
         <Route path={SIGN_IN.INDEX} element={<SignIn />} />
+        <Route path={ONBOARDING.INDEX} element={<Onboarding />} />
       </Routes>
     </Router>
   )
