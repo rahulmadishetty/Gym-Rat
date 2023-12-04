d9bcab011bed6302df35297358220f837714af57
Anisha Pant <annray@Moon.local>
2023-12-03 22:51:55 -0500

    Update design

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,16 +1,43 @@
 import React, { useState, useEffect } from 'react'
 
 import Header from '../components/Layout/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-import BaseRequest from "../services/requests/Base"
+import BaseRequest from "../services/requests/Base";
+
+const getAge = (age) => {
+    if (age == "below30") {
+        return "18-29"
+    } else if (age == "below40") {
+        return "30-39"
+    } else if (age == "below50") {
+        return "40-49"
+    } else return "50+"
+}
+
+const getGoal = (goal) => {
+    if (goal == "loseweight") {
+        return "Lose Weight"
+    } else if (goal == "gainmuscle") {
+        return "Gain Muscle"
+    } else return "Get Shredded"
+}
+
+const getBodyType = (goal) => {
+    if (goal == "ectomorph") {
+        return "Ectomorph"
+    } else if (goal == "mesomorph") {
+        return "Mesomorph"
+    } else return "Endomorph"
+}
 
 const Profile = () => {
-
     const [userData, setUserData] = useState({
         fName: '',
-        dName:"",
+        dName: "",
         email: '',
         age: '',
         goal: '',
@@ -23,16 +50,15 @@ const Profile = () => {
         const fetchUserData = async () => {
             try {
                 const response = await BaseRequest.getAuthenticated(`http://localhost:3000/profile/${userId}`)
-                
                 const user = response.data.userProfile;
-                
+
                 setUserData({
                     fName: user.fName,
                     email: user.email,
-                    age: user.age,
-                    goal: user.goal,
-                    bodyType: user.bodyType,
-                    dName:user.dName
+                    age: getAge(user.age),
+                    goal: getGoal(user.goal),
+                    bodyType: getBodyType(user.bodyType),
+                    dName: user.dName
                 });
             } catch (error) {
                 console.error('Error fetching user data:', error);
@@ -40,30 +66,6 @@ const Profile = () => {
         };
         fetchUserData();
     }, []);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUserData({
-            ...userData,
-            [name]: value,
-        });
-    };
-
-    // UpdateProfileHandler = (e) => {
-    //     e.preventDefault();
-    //     //create object of form data
-    //     const formData = new FormData();
-
-    //     //update-profile
-    //     axios.post("http://localhost:3000/profile/create", formData, {
-    //         headers: {
-    //             "content-type": "application/json"
-    //         }
-    //     }).then(res => {
-    //         console.log(res);
-    //     })
-    //         .catch(err => console.log(err))
-    // }
 
     return (
         <div>
@@ -74,42 +76,47 @@ const Profile = () => {
                     <Col>
                     </Col>
                     <Col>
-                    <br></br>
-                    <br></br>
-                        <h1>User Profile</h1>
-                        <Form className="form">
-                            <p>Welcome</p>
+                        <br></br>
+                        <br></br>
+                        <Form className="form mt-5">
+                            <div className='d-flex justify-content-between align-items-center mb-5'>
+                                <h3 className='color-secondary'>User Profile</h3>
+                                <div className='color-secondary'>
+                                    <FontAwesomeIcon icon={faPenToSquare} /> Edit Profile
+                                </div>
+
+                            </div>
+
                             <Form.Group controlId="formCategory1">
                                 <Form.Label>Full Name</Form.Label>
-                                <Form.Control type="text" defaultValue={userData.fName} disabled />
+                                <Form.Control type="text" className="color-gray mb-3" defaultValue={userData.fName} disabled />
 
                             </Form.Group>
-                            <Form.Group controlId="formCategory1">
+                            <Form.Group controlId="formCategory2">
                                 <Form.Label>Display Name</Form.Label>
-                                <Form.Control type="text" defaultValue={userData.dName} disabled />
+                                <Form.Control className="color-gray mb-3" type="text" defaultValue={userData.dName} disabled />
 
                             </Form.Group>
 
-                            <Form.Group controlId="formCategory2">
+                            <Form.Group controlId="formCategory3">
                                 <Form.Label>Age</Form.Label>
-                                <Form.Control type="text" defaultValue={userData.age} disabled />
+                                <Form.Control type="text" className="color-gray mb-3" defaultValue={userData.age} disabled />
 
                             </Form.Group>
 
-                            <Form.Group controlId="formCategory2">
+                            <Form.Group controlId="formCategory4">
                                 <Form.Label>Goal</Form.Label>
-                                <Form.Control type="text" defaultValue={userData.goal} disabled />
+                                <Form.Control type="text" className="color-gray mb-3" defaultValue={userData.goal} disabled />
 
                             </Form.Group>
 
-                            <Form.Group controlId="formCategory2">
+                            <Form.Group controlId="formCategory5">
                                 <Form.Label>Body Type</Form.Label>
-                                <Form.Control type="text" defaultValue={userData.bodyType} disabled />
+                                <Form.Control type="text" className="color-gray mb-3" defaultValue={userData.bodyType} disabled />
 
                             </Form.Group>
 
                             <br></br>
-                            <Button variant="primary">Update Profile</Button>
                         </Form>
                     </Col>
                     <Col>
